2de0f8e9e387c06cde7313fb0755bbd01b2f7bee
vinaychandranp <vinaychandranp@gmail.com>
2016-02-18 13:21:53 +0100

    added functionality

--- baseline_clustering.py
+++ baseline_clustering.py
@@ -11,10 +11,20 @@ container = 'testing_set_01.hdf5'
 mnist = h5py.File(container)
 
 mnist_estimator = cluster.KMeans(10, n_jobs=-1)
-mnist_estimator.fit(mnist['feats_1'])
 
-print('Clustering performance evaluation')
+
+mnist_estimator.fit(mnist['feats_1'])
+print('Clustering performance evaluation - feats_1')
 print('Homogeneity Score: '+ str(metrics.homogeneity_score(mnist['label'],mnist_estimator.labels_)))
 print('Completeness Score: '+ str(metrics.completeness_score(mnist['label'],mnist_estimator.labels_)))
 print('Adjusted Mutual Information Score: '+  str(metrics.adjusted_mutual_info_score(mnist['label'],mnist_estimator.labels_)))
 print('Adjusted Rand Index Score: '+  str(metrics.adjusted_rand_score(mnist['label'],mnist_estimator.labels_)))
+
+
+mnist_estimator.fit(mnist['feats_2'])
+print('Clustering performance evaluation - feats_2')
+print('Homogeneity Score: '+ str(metrics.homogeneity_score(mnist['label'],mnist_estimator.labels_)))
+print('Completeness Score: '+ str(metrics.completeness_score(mnist['label'],mnist_estimator.labels_)))
+print('Adjusted Mutual Information Score: '+  str(metrics.adjusted_mutual_info_score(mnist['label'],mnist_estimator.labels_)))
+print('Adjusted Rand Index Score: '+  str(metrics.adjusted_rand_score(mnist['label'],mnist_estimator.labels_)))
+
