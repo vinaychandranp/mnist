587f085dacbc9f44e7716451e02bd01de9cd49e1
vinaychandranp <vinaychandranp@gmail.com>
2016-03-01 10:30:41 +0100

    bug fix

--- baseline_clustering.py
+++ baseline_clustering.py
@@ -9,22 +9,22 @@ import argparse
 from sklearn import cluster
 from sklearn import metrics
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-tr', '--train', metavar='FILE', dest='trainfile',
-#                     required=False, default=None,
-#                     help='HDF5 container for train data')
-# parser.add_argument('-te', '--test', metavar='FILE', dest='testfile',
-#                     help='HDF5 container for test data')
-# parser.add_argument('-nc', '--num_class', metavar='FILE', required=False,
-#                     dest='nc', default=10, help='Number of classes ')
-# opts = parser.parse_args(sys.argv[1:])
-# container_train = opts.trainfile
-# container_test = opts.testfile
-# nc = int(opts.nc)
+parser = argparse.ArgumentParser()
+parser.add_argument('-tr', '--train', metavar='FILE', dest='trainfile',
+                    required=False, default=None,
+                    help='HDF5 container for train data')
+parser.add_argument('-te', '--test', metavar='FILE', dest='testfile',
+                    help='HDF5 container for test data')
+parser.add_argument('-nc', '--num_class', metavar='FILE', required=False,
+                    dest='nc', default=10, help='Number of classes ')
+opts = parser.parse_args(sys.argv[1:])
+container_train = opts.trainfile
+container_test = opts.testfile
+nc = int(opts.nc)
 
-container_train = sys.argv[1]
-container_test = sys.argv[2]
-nc = int(sys.argv[3])
+# container_train = sys.argv[1]
+# container_test = sys.argv[2]
+# nc = int(sys.argv[3]x)
 
 # container_train = 'training_set_01.hdf5'
 # container_test = 'testing_set_01.hdf5'
@@ -36,7 +36,7 @@ mnist_test = h5py.File(container_test, 'r')
 def purity(truth, pred):
     conf = metrics.confusion_matrix(truth, pred)
     t = 0
-    for i in range(0, mnist_train):
+    for i in range(0, nc):
         t += np.amax(conf[i, :])
     return float(t)/np.sum(conf)
 
